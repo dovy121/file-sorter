4709c3fa913f53bfc5e37669fcbaed4bbb0e218d
dovy121 <81447981+dovy121@users.noreply.github.com>
2021-03-27 00:53:22 +0000

    added first lines

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 import os
+from PIL import Image
+from PIL.ExifTags import TAGS
+import shutil
+
+doc_ext = [".doc", ".docx", ".txt"]
+img_ext = [".png", ".jpeg", ".jpg", ".bmp"]
 
 sort_path = input("Please place path which will be used to sort in: ")
+item_list = os.listdir(sort_path)
+
+for item in item_list:
+    file = sort_path +"\\"+item
+    if os.path.isdir(file):
+       os.mkdir(sort_path + "\\Folders")
+       shutil.move(file, sort_path + "\\Folders\\" + item)
+    if os.path.isfile(file)
+        #need to finish here
+
+
+
+
+
+
+
+
+print(new_list)
+
+
