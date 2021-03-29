fba14b7e9c7841a1ba9e3ee1d5f811bed45cf5b8
dovy121 <81447981+dovy121@users.noreply.github.com>
2021-03-29 01:17:16 +0100

    Started creating function for creating folders.

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from scan_path import *
 from get_creation_date import *
 
-#mypath=r"E:\My Files"
-#sf, doc_files, img_files, oth_files = scan_path(mypath)
+mypath=r"E:\My Files"
+sf, doc_files, img_files, oth_files = scan_path(mypath)
+
+for i in img_files:
+    print(creation_date(i))
 
 # path_to_file = r"E:\My Files\2020-07-08 10_40_10-Window.jpg"
 # local_time = creation_date(path_to_file)
